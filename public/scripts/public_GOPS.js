5eab3b6cb19b0fea2106575228b6fb4f8603ffaa
Evgenyg <evgenyg@live.com>
2017-09-28 15:37:21 -0600

    Modified functions, push pre-merge

--- public/scripts/public_GOPS.js
+++ public/scripts/public_GOPS.js
@@ -1,25 +1,3 @@
-const card_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-//May not need, can maybe random cards drawn from player hand instead? maybe easier?
-// function shuffle_array(array) {
-//   for (let i in array) {
-//     let j = Math.floor(Math.random() * i);
-//     console.log(j);
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-// }
-
-
-function create_gop() {
-  const user_1 = card_values;
-  const user_2 = card_values;
-
-
-}
-
-
-
-
 //For when the window closes, ask if forfight
 $(window).on("beforeunload", function() {
   return inFormOrLink ? "Do you really want to close?" : null;
@@ -36,3 +14,7 @@ window.onbeforeunload = function(evt) {
 
   return message;
 };
+
+
+
+//should only have view function, what it gets back from server
