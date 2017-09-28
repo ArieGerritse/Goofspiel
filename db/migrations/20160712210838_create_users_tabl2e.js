9b5cb6a517b4d914f668da9b66f77a9b965124d6
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-28 11:38:24 -0600

    Migrations v2

---
+++ db/migrations/20160712210838_create_users_tabl2e.js
@@ -0,0 +1,24 @@
+xports.up = function(knex, Promise) {
+  return knex.schema.createTable("user_data", (table) => {
+    table.increments();
+    table.string('Username');
+    table.string('Email');
+    table.string('Password');
+  })
+  knex.schema.createTable("game_history", (table) => {
+    table.increments();
+    table.string('User1_ID');
+    table.string('User2_ID');
+    table.string('Winner');
+    table.date('Date');
+  })
+  knex.schema.createTable("game_goofspiel", (table) => {
+    table.increments();
+    table.string('Last Move Player');
+    table.string('List of Previous Movies');
+  })
+  knex.schema.dropTable('User Data');
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('dser_data', 'game_history', 'game_goofspiel');
+};
