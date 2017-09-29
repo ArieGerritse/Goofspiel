56158c8c5b21618e53b5a29b126d505d5e1ed3f8
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-28 18:27:06 -0600

    Alex Migration

--- db/migrations/20170929000302_alex_migration.js
+++ db/migrations/20170929000302_alex_migration.js
@@ -1,5 +1,17 @@
 exports.up = function(knex, Promise) {
 
+  return knex.schema.createTable("game_hand", (table) => {
+      table.increments();
+      table.integer('game_id');
+      table.integer('user_id');
+      talbe.integer('score');
+    }),
+    knex.schema.createTable("cards_played", (table) => {
+      table.increments();
+      table.integer('hand_id');
+      talbe.integer('value');
+    });
+
   return knex.schema.createTable("user", (table) => {
       table.increments();
       table.integer('games_won');
