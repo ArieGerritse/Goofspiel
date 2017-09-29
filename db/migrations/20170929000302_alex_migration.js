7051209f391d44fd667b4fbda35497871e6a0527
Evgenyg <evgenyg@live.com>
2017-09-28 18:26:18 -0600

    Migration commit 1

--- db/migrations/20170929000302_alex_migration.js
+++ db/migrations/20170929000302_alex_migration.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
+
+  return knex.schema.createTable("user", (table) => {
+      table.increments();
+      table.integer('games_won');
+      table.integer('is_dealer');
+    }),
+    knex.schema.createTable("current_game", (table) => {
+      table.increments();
+      table.text('winner');
+      table.integer('turn_count');
+    }),
+    knex.schema.dropTable('User Data');
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('game_data', 'current_game', 'game_hand', 'cards_played');
 };
