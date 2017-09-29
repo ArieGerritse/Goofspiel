05e1e38c8320eb4c60c61c5ed51538a66ff02fc6
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-29 14:23:15 -0600

    FIxed Incrementing Seed Problem

--- db/seeds/cards.js
+++ db/seeds/cards.js
@@ -3,32 +3,26 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return Promise.all([
         knex('cards_played').insert({
-          id: 1,
           hand_id: 1,
           value: 6
         }),
         knex('cards_played').insert({
-          id: 2,
           hand_id: 2,
           value: 7
         }),
         knex('cards_played').insert({
-          id: 3,
           hand_id: 3,
           value: 3
         }),
         knex('cards_played').insert({
-          id: 4,
           hand_id: 1,
           value: 8
         }),
         knex('cards_played').insert({
-          id: 5,
           hand_id: 2,
           value: 10
         }),
         knex('cards_played').insert({
-          id: 6,
           hand_id: 3,
           value: 12
         })
