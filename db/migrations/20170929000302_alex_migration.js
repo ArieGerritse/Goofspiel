37b6afe2046041c734b0c8b06fbde68d03d65053
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-28 18:51:59 -0600

    Fixed Down on Migration

--- db/migrations/20170929000302_alex_migration.js
+++ db/migrations/20170929000302_alex_migration.js
@@ -1,12 +1,8 @@
 exports.up = function(knex, Promise) {
 
-  return knex.schema.dropTable('game_data');
 };
 
 exports.down = function(knex, Promise) {
 
-  return knex.schema.createTable("game_data", (table) => {
-    table.increments();
-  });
 
 };
